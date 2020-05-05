fd54106aeef7a6950a5c39564187cb0df2cfc425
alexbobylewa <alexbobylewa@gmail.com>
2020-05-05 10:54:41 +0300

    Fix build

--- build.ps1
+++ build.ps1
@@ -1,25 +1,2 @@
-
-$result = Invoke-Pester "Get-Planet.Tests.ps1"
-
-if ($result.TotalCount -lt 1) {
-    $m = "No tests were run."
-
-    if ($LocalBuild) {
-        $m
-        exit 9999
-    }
-    else {
-        throw $m
-    }
-}
-
-if ($result.FailedCount -gt 0) {
-    $m = "$($result.FailedCount) tests did not pass."
-    if ($LocalBuild) {
-        $m
-        exit $result.FailedCount
-    }
-    else {
-        throw $m
-    }
-}
+ Write-Output $PSScriptRoot
+$result = Invoke-Pester "$PSScriptRoot\Get-Planet.Tests.ps1"
